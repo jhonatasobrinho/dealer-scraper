762103e8397452078ebabd7ec4ed0e5d2782007f
Jhonata Sobrinho <jhonata.sobrinho@mercadolivre.com>
2021-02-20 23:08:59 -0300

    Remove peek on overly positive reviews usecase

--- src/main/java/com/dealerscraper/usecases/OverlyPositiveReviews.java
+++ src/main/java/com/dealerscraper/usecases/OverlyPositiveReviews.java
@@ -17,7 +17,6 @@ public class OverlyPositiveReviews {
     public List<Evaluation> retrieveTopThreeEvaluations(final Set<ReviewEntry> reviewEntries) {
         return reviewEntries.stream()
                 .map(reviewEntry -> new Evaluation(evaluator.evaluate(reviewEntry), reviewEntry))
-                .peek(System.out::println)
                 .sorted()
                 .limit(3)
                 .collect(Collectors.toUnmodifiableList());
